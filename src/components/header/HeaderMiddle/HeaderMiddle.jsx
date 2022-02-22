c6e28587b12df5d1f87158f11bc2a303ae235bb9
Pavel Serzhan <p.i.serzhan@gmail.com>
2022-02-22 10:55:57 +0300

    feat: logo link added

--- src/components/header/HeaderMiddle/HeaderMiddle.jsx
+++ src/components/header/HeaderMiddle/HeaderMiddle.jsx
@@ -1,17 +1,18 @@
 import { Contacts } from "./Contacts"
 import { Basket } from "./Basket"
+import { Link } from "react-router-dom"
 
 export const HeaderMiddle = ({ data, logo }) => {
   return (
     <div className="header__middle">
       <div className="container header__container header__container_middle">
         <div className="header__col header__col_logo">
-          <a href="!#" className="header__logo logo">
+          <Link to="*" className="header__logo logo">
             <img src={logo && logo.link}
               id={logo && logo.id}
               className="logo__img"
               alt={logo && logo.alt}></img>
-          </a>
+          </Link>
         </div>
         <Contacts data={data} />
         <Basket />
