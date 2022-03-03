2aea74463a3cf8f5d7d71e3d5f0f9975d6391e15
Pavel Serzhan <p.i.serzhan@gmail.com>
2022-03-03 11:42:42 +0300

    feat: gallery fixes

--- src/components/main/Product/Card/Gallery.jsx
+++ src/components/main/Product/Card/Gallery.jsx
@@ -1,35 +1,33 @@
-export const Gallery = ({ data, id }) => {
-  return (
-    <div className="card__photos gallery">
-      <div className="gallery__display">
-        <img
-          src={data && data[0]}
-          width={570}
-          height={525}
-          alt=""
-          className="gallery__display-img"
-        />
-      </div>
-      <div className="gallery__thumbnails-container">
-        <ul className="gallery__thumbnails">
-          <li className="gallery__thumbnails-item gallery__thumbnails-item_active">
+export const Gallery = ({ data, id }) => (
+  <div className="card__photos gallery">
+    <div className="gallery__display">
+      <img
+        src={data && data[0]}
+        width={570}
+        height={525}
+        alt=""
+        className="gallery__display-img"
+      />
+    </div>
+    <div className="gallery__thumbnails-container">
+      <ul className="gallery__thumbnails">
+        <li className="gallery__thumbnails-item gallery__thumbnails-item_active">
+          <img
+            src={data && data[0]}
+            alt=""
+            className="gallery__thumbnails-img"
+          />
+        </li>
+        {data && data.slice(1, data.length - 1).map(el => (
+          <li key={el} className="gallery__thumbnails-item">
             <img
-              src={data && data[0]}
+              src={el}
               alt=""
               className="gallery__thumbnails-img"
             />
           </li>
-          {data && data.slice(1, data.length - 1).map((el, i) => (
-            <li key={id + i} className="gallery__thumbnails-item">
-              <img
-                src={el}
-                alt=""
-                className="gallery__thumbnails-img"
-              />
-            </li>
-          ))}
-        </ul>
-      </div>
+        ))}
+      </ul>
     </div>
-  )
-}
+  </div>
+)
