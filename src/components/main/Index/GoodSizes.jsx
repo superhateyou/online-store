cc5af332f5d1527cd4a8ecdec827962366bb06b4
Pavel Serzhan <p.i.serzhan@gmail.com>
2022-03-03 10:53:14 +0300

    fix: sizes render method fix

--- src/components/main/Index/GoodSizes.jsx
+++ src/components/main/Index/GoodSizes.jsx
@@ -1,32 +1,13 @@
 export const GoodSizes = ({ data, id }) => {
-  if (data.isStock === true) {
-    return (
-      <div className="checkbox-tile" key={`${data.id}${id}`}>
-        <input
-          id={`${data.id}-size${data.size}`}
-          name={`${data.id}[${data.size}]`}
-          type="radio"
-          defaultValue={data.size}
-          required=""
-          className="checkbox-tile__elem"
-        />
-        <label
-          htmlFor={`${data.id}-size${data.size}`}
-          className="checkbox-tile__label"
-        >
-          {data.size}
-        </label>
-      </div>
-    )
-  } else return (
-    <div className="checkbox-tile" key={`${data.id}${data.size}`}>
+  return (
+    <div className="checkbox-tile" key={`${data.id}${id}`}>
       <input
         id={`${data.id}-size${data.size}`}
         name={`${data.id}[${data.size}]`}
         type="radio"
         defaultValue={data.size}
-        disabled
         required=""
+        disabled={data.isStock}
         className="checkbox-tile__elem"
       />
       <label
@@ -37,4 +18,4 @@ export const GoodSizes = ({ data, id }) => {
       </label>
     </div>
   )
-}
+} 
