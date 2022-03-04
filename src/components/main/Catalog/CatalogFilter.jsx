8baa1c5ee66d080003ca040574c2fa9556fc2963
Pavel Serzhan <p.i.serzhan@gmail.com>
2022-03-04 17:43:52 +0300

    feat: prices filter added

--- src/components/main/Catalog/CatalogFilter.jsx
+++ src/components/main/Catalog/CatalogFilter.jsx
@@ -4,6 +4,7 @@ import { ColorFilter } from "./ColorFilter"
 import { GenderFilter } from "./GenderFilter"
 import { SizesFilter } from "./SizesFilter"
 import { AvailabilityFilter } from "./AvailabilityFilter"
+import { PriceFilter } from "./PriceFilter"
 
 export const CatalogFilter = ({ data }) => {
   return (
@@ -21,28 +22,7 @@ export const CatalogFilter = ({ data }) => {
         <GenderFilter data={data && data} />
         <SizesFilter data={data && data} />
         <AvailabilityFilter />
-        <div className="form__row form__row_direction_column">
-          <label className="form__label">Цена, руб.</label>
-          <div className="range-slider">
-            <input className="range-slider__elem" type="text" />
-            <div className="range-slider__output-row">
-              <input
-                name="Filter[price-min]"
-                data-type="min"
-                readOnly=""
-                className="input range-slider__output"
-                type="text"
-              />
-              <input
-                name="Filter[price-max]"
-                data-type="max"
-                readOnly=""
-                className="input range-slider__output"
-                type="text"
-              />
-            </div>
-          </div>
-        </div>
+        <PriceFilter data={data && data} />
         <button type="submit" className="btn">
           Показать товары
         </button>
--- src/components/main/Catalog/SeasonFilter.jsx
+++ src/components/main/Catalog/SeasonFilter.jsx
@@ -2,7 +2,6 @@ export const SeasonFilter = ({ data }) => {
   const seasons = data?.map(el => el.season)
     .reduce((prev, curr) => prev.concat(curr), [])
     .filter((e, i, a) => a.indexOf(e) === i)
-  console.log(seasons)
   return (
     <div className="form__row form__row_direction_column">
       <label className="form__label">Сезон</label>
